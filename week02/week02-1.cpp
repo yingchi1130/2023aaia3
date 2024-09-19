bada4f184e94572e7f18240a1e0b373517e3e9b7
yingchi1130 <kiki001130@gmail.com>
2024-09-19 14:38:35 +0800

    week02

---
+++ week02/week02-1.cpp
@@ -0,0 +1,15 @@
+//week02-1.cpp
+//SOIT107_ADVANCE_006
+//not ok
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s;
+	
+	cout << s;
+	cout << "\n";
+}

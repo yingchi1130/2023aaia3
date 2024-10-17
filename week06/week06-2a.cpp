e4e05b16e09ba1f3808dcbd2713874a818752392
yingchi1130 <kiki001130@gmail.com>
2024-10-17 15:37:06 +0800

    week06

---
+++ week06/week06-2a.cpp
@@ -0,0 +1,12 @@
+//week06-2a.cpp (only Step01 Step02)
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+	int now;
+	while( cin >> now ){ //Step01 Input
+		
+		cout << now << "\n"; //Step02 Output
+	}
+}
---
+++ week06/week06-2b.cpp
@@ -0,0 +1,15 @@
+//week06-2b.cpp voclor arry Step03 Step04
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+	vector<int> a; //Step03 vector arry
+	int now;
+	while( cin >> now ){ //Step01 Input
+		a.push_back(now); //Step03 vector array
+		for(int b:a) cout << b << ' ';
+		cout << "\n";  //Step04 print.array
+		//cout << now << "\n"; //Step02 Output
+	}
+}

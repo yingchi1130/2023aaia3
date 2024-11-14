1c3e680ff30e47271a9b8ca709ea4af5ae5028bf
yingchi1130 <kiki001130@gmail.com>
2024-11-14 14:25:48 +0800

    week10

---
+++ week10/week10-1.cpp
@@ -0,0 +1,27 @@
+///week10-1.cpp 要教類別class
+#include <iostream>
+using namespace std;
+///你在寫class Cat {寫完大括號,他會幫你補完}; 再多打2個Enter
+class Cat{ ///貓的類別
+public:
+    void print(){
+        cout << "I am a cat. meow meow\n";
+    }
+};
+
+class Mouse{
+public:
+    void print(){
+        cout << "I am a mouse. chi chi\n";
+    }
+};
+
+int main()
+{///大寫 小寫
+    Cat cat1,cat2; ///cat1 cat2 都是 Cat
+    cat1.print();
+    cat2.print();
+    Mouse mouse1, mouse2;
+    mouse1.print();
+    mouse2.print();
+}

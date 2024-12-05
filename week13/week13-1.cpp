307078b4b91631026029d0952a232481cd372704
yingchi1130 <kiki001130@gmail.com>
2024-12-05 14:18:28 +0800

    week13

---
+++ week13/week13-1.cpp
@@ -0,0 +1,19 @@
+//week13-1.cpp UVA10035 not Ok
+#include <iostream>
+using namespace std;
+int helper(int a, int b) //Step03: function
+{
+	return 3;
+}
+int main()
+{
+	int a,b;
+	while(cin >> a >> b){ //Step01:Input
+		if(a==0 && b==0) break;
+		
+		int ans = helper(a,b); //Step03: function
+		if(ans==0) cout << "No carry operation.\n";
+		else if(ans==1) cout << "1 carry operation.\n";
+		else cout << ans << " carry operation.\n";
+	}//Step02: Output 
+}

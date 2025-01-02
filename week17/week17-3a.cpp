990901a2f359d07756919b3ddd330a2bb2c49af2
yingchi1130 <kiki001130@gmail.com>
2025-01-02 16:05:55 +0800

    week17

---
+++ week17/week17-3a.cpp
@@ -0,0 +1,17 @@
+//week17-3a.cpp SOIT107_ADVANCE_005
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string a,b;
+	cin >> a;
+	cin >> b;
+	int N1 = a.length(), N2 = b.length();
+	if(N1>N2) cout << 1;
+	else if(N1<N2) cout << -1;
+	else{
+		//c: strcmp() c++:string.compare()
+		cout << a.compare(b);		
+	}
+}

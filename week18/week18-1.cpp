d590df8e6723769415eceed12dbd7085ec8b1916
yingchi1130 <kiki001130@gmail.com>
2025-01-09 14:08:34 +0800

    week18

---
+++ week18/week18-1.cpp
@@ -0,0 +1,22 @@
+//week18-1.cpp
+//LeetCode 2185. Counting Words With a Given Prefix
+//挑戰題
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+        int ans=0;
+        //for(int i=0; i<words.size; i++){
+        // words[i]
+        for(string word : words){
+            int bad=0;
+            for(int i=0;pref[i]!=0;i++){
+                if(pref[i] != word[i]){ 
+                    bad=1;
+                    break;
+                }
+            }
+            if(bad==0) ans++;
+        }
+        return ans;
+    }
+};
